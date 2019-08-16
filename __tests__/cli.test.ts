890646f5417a60638cf9a9bcdd60f7f7b4c6a5ce
yami-beta <yami-beta@users.noreply.github.com>
2019-08-16 11:43:19 +0900

    feat: Add frontend boilerplate

--- __tests__/cli.test.ts
+++ __tests__/cli.test.ts
@@ -4,12 +4,62 @@ import sao from "sao";
 const generator = path.resolve(__dirname, "..", "lib");
 
 describe("create-ts-app", () => {
-  test.each<[string]>([["README.md"], ["package.json"]])(
-    "it should include %s",
-    async filename => {
-      const mockPromptAnswers = { name: "test-app" };
-      const stream = await sao.mock({ generator }, mockPromptAnswers);
-      expect(stream.fileList).toContain("README.md");
-    }
-  );
+  test.each<[string]>([
+    ["README.md"],
+    ["LICENSE"],
+    ["package.json"],
+    ["yarn.lock"],
+    ["tsconfig.json"],
+    ["webpack.config.js"],
+    ["src/index.html"],
+    ["src/index.tsx"],
+    [".gitignore"],
+    [".eslintignore"],
+    [".eslintrc.js"],
+    [".prettierrc.js"]
+  ])("it should include %s", async filename => {
+    const mockPromptAnswers = {
+      appType: "frontend",
+      features: ["eslint", "prettier"],
+      name: "test-app",
+      license: "MIT"
+    };
+    const stream = await sao.mock({ generator }, mockPromptAnswers);
+    expect(stream.fileList).toContain(filename);
+  });
+
+  describe("appType is frontend", () => {
+    describe("ESLint is not selected", () => {
+      test.each<[string]>([[".eslintignore"], [".eslintrc.js"]])(
+        "it should not include %s",
+        async filename => {
+          const mockPromptAnswers = {
+            appType: "frontend",
+            features: ["prettier"],
+            name: "test-app",
+            license: "MIT"
+          };
+          const stream = await sao.mock({ generator }, mockPromptAnswers);
+          expect(stream.fileList).not.toContain(filename);
+        }
+      );
+    });
+
+    describe("ESLint and Prettier are not selected", () => {
+      test.each<[string]>([
+        [".eslintignore"],
+        [".eslintrc.js"],
+        [".prettierrc.js"]
+      ])("it should not include %s", async filename => {
+        const mockPromptAnswers = {
+          appType: "frontend",
+          features: [],
+          name: "test-app",
+          license: "MIT"
+        };
+        const stream = await sao.mock({ generator }, mockPromptAnswers);
+        expect(stream.fileList).not.toContain(filename);
+      });
+    });
+  });
 });
