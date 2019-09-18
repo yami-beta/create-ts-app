62ee1ed56d5f4f7d5c41ff2bdf9c71ed6f5424c2
yami-beta <yami-beta@users.noreply.github.com>
2019-09-18 12:52:24 +0900

    chore: add eslint-plugin-react-hooks

--- template/frontend/.eslintrc.js
+++ template/frontend/.eslintrc.js
@@ -5,17 +5,19 @@ module.exports = {
     commonjs: true,
     es6: true
   },
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  plugins: ["@typescript-eslint"],
+  rules: {
+    // interface や jsx をうまく扱えないので off にして noUnusedLocals を tsconfig で有効化
+    "no-unused-vars": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     sourceType: "module"
-  },
-  rules: {
-    // interface や jsx をうまく扱えないので off にして noUnusedLocals を tsconfig で有効化
-    "no-unused-vars": "off"
   }
 };
